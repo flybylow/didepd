a7f4dc75a4d44d36431cb82ac29d6aa34504c47b
flybylow <flybylow@users.noreply.github.com>
2026-05-12 15:11:06 +0200

    Update package.json to include jsonld override and modify next.config.ts to set assetPrefix for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: "https://didepd.vercel.app",
   images: { unoptimized: true },
   reactCompiler: true,
   transpilePackages: [
